64735e07410af58724f0e85a730d32b2790e9b7a
Caio Machado de Oliveira <caiomoliveira@hotmail.com>
2022-01-05 19:39:40 +0000

    adding code to index.js

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ const   http = require('http'),
 const   router = express(), 
         server = http.createServer(router);
 
+router.use(express.static(path.resolve(__dirname,'html'))); //We serve static content from "html" folder
+router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
+router.use(express.json()); //We include support for JSON
+
+// Function to read in XML file and convert it to JSON
+function XMLtoJSON(filename, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+      if (err) throw (err);
+      xml2js.parseString(xmlStr, {}, cb);
+    });
+};
+  
+  //Function to convert JSON to XML and save it
+function JSONtoXML(filename, obj, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+    fs.unlinkSync(filepath);
+    fs.writeFile(filepath, xml, cb);
+};
+
+
         
 router.get('/', function (req, res) {
         res.writeHead(200, {'Content-Type' : 'text/html'});
