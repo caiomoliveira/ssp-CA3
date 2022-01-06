e9ad98a2c5cfcff97bb309b97a8feaa599a7b408
Caio Machado de Oliveira <caiomoliveira@hotmail.com>
2022-01-06 12:48:31 +0000

    setting css and js in the html page

--- views/js/table.js
+++ views/js/table.js
@@ -30,24 +30,41 @@ function select_row()
 	})
 };
 
-function delete_row(sec, ent)
-{
-	$("#delete").click(function ()
-	{
-		$.ajax(
-		{
-			url: "/post/delete",
-			type: "POST",
-			data:
-			{
-				section: sec,
-				entree: ent
-			},
-			cache: false,
-			success: setTimeout(draw_table, 1000)
-		})
-	})
-};
+/*---------------------------------------------*/
+checkpassword = function(validade){
+    var str = document.getElementById().value;
+    if(str.lenght < 8)
+    {
+        document.getElementById("demo").innerHTML= "Password lenght most be 8 char";
+        document.getElementById("demo").style.color="Red";
+        return("too_short");
+
+    } else if (str.search(/[0-9]/) == -1) {
+        document.getElementById("demo").innerHTML= "At least 1 numeric value most be enter";
+        document.getElementById("demo").style.color="Red";
+        return("no_number");
+    }
+    else if (str.search(/[a-z]/) == -1) {
+        document.getElementById("demo").innerHTML= "At least 1 letter  most be enter";
+        document.getElementById("demo").style.color="Red";
+        return("no_letter");
+    }
+    else if (str.search(/[A-Z]/) == -1) {
+        document.getElementById("demo").innerHTML= "At least 1 Upper case letter  most be enter";
+        document.getElementById("demo").style.color="Red";
+        return("no_Uletter");
+    }
+    else if (str.search(/[!\@\#\$\%\¨\&\*\-\+\.\;\,\:]/) == -1) {
+        document.getElementById("demo").innerHTML= "At least 1 special char  most be enter";
+        document.getElementById("demo").style.color="Red";
+        return("no_Schar");
+    }
+        document.getElementById("demo").innerHTML= "Successful";
+        document.getElementById("demo").style.color="Green";
+        return("ok");
+    
+}
+
 
 $(document).ready(function ()
 {
